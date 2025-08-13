69ae530069fd0e41a3c94a6a369247a279921add
zds-d <1050296225@qq.com>
2025-08-13 08:30:51 +0800

    feat:  富婆排行榜

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -12,14 +12,6 @@ const projectsData: Project[] = [
     imgSrc: '/static/images/projects/rich-woman/code.jpg',
     href: 'blog/projects/rich-woman',
   },
-  {
-    title: '2The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
 ]
 
 export default projectsData
