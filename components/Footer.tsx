e9fa1353c3024a14e98cef657833d52c2428d624
zds-d <1050296225@qq.com>
2025-08-14 03:27:49 +0800

    feat: 本地特色

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,7 +26,11 @@ export default function Footer() {
           <div>{` • `}</div>
           <Link href="/">{siteMetadata.title}</Link>
         </div>
-
+        <div className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
+            Tailwind Nextjs Theme
+          </Link>
+        </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           <Link href={siteMetadata.beian.link} target="_blank" rel="noopener noreferrer">
             {siteMetadata.beian.number}
