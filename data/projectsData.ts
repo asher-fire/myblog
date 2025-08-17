eb7c98374d0727731d334bc96c61435a2995b67a
zds-d <1050296225@qq.com>
2025-08-17 21:36:19 +0800

    feat(app): 本地特色

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -10,7 +10,7 @@ const projectsData: Project[] = [
     title: '富婆排行榜',
     description: `一个玩梗类的微信小程序，当有人说不想努力的时候，抛出该小程序，赚取可怜的微信广告费用。`,
     imgSrc: '/static/images/projects/rich-woman/code.jpg',
-    href: 'blog/projects/rich-woman',
+    href: 'blog/works/rich-woman',
   },
 ]
 
